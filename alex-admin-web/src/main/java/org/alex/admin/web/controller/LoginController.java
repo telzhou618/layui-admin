d9b8c64a5294e09d10dee5e8d7b1f5a6557c8990
gaojun.zhou <gaojun.zhou@qq.com>
2017-07-05 18:19:37 +0800

    开发登录模块

--- alex-admin-web/src/main/java/org/alex/admin/web/controller/LoginController.java
+++ alex-admin-web/src/main/java/org/alex/admin/web/controller/LoginController.java
@@ -1,35 +1,72 @@
 package org.alex.admin.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.alex.admin.core.bean.Rest;
+import org.alex.admin.web.entity.SysUser;
+import org.alex.admin.web.service.ISysUserService;
+import org.alex.admin.web.util.BaseUtil;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 /**
  * 标准的Rest接口,实例控制器
  * Created by Gaojun.Zhou 2017年6月8日
  */
-@RestController
-@RequestMapping("/login")
+@Controller
 public class LoginController{   
+	
+	@Autowired private ISysUserService sysUserService;
 	
 	/**
 	 * 登录
 	 * @return
 	 */
-	@PostMapping("/login")
-	public Rest login(){
+	@GetMapping("/login")
+	public String login(){
+		return "login";
 		
-		return Rest.ok();
+	}
+	
+	/**
+	 * 执行登录
+	 * @param username
+	 * @param password
+	 * @param request
+	 * @return
+	 */
+	@ResponseBody
+	@PostMapping("/doLogin")
+	public Rest doLogin(String username,String password,HttpServletRequest request){
+		
+		if(StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+			throw new RuntimeException("用户名或密码不能为空");
+		}
+		SysUser user = sysUserService.selectOne(new EntityWrapper<SysUser>().eq("userName",username).eq("password",BaseUtil.MD5(password)));
+		if(user == null){
+			throw new RuntimeException("用户名或密码错误");
+		}
+		if(user.getUserState() == -1){
+			throw new RuntimeException("该用户已锁定,请联系超级管理员");
+		}
+		
+		request.getSession().setAttribute("session_user",user);
+		return Rest.ok("登录成功");
+		
 	}
 	
 	/**
 	 * 注销
 	 * @return
 	 */
-	@GetMapping("/lougout")
-    public Rest logout(){
-    	
-    	return Rest.ok();
+	@GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+		request.getSession().invalidate();
+		return "redirect:login";
     }
 }
